6b915ae42b4e79e1dfc9eab2304a27831a226de3
neetdai <neetdai>
2023-07-31 14:50:35 +0800

    ✨ feat:
    add read_at and write trait; impl for file

--- src/fs.rs
+++ src/fs.rs
@@ -1,6 +1,5 @@
 use windows_sys::Win32::Foundation::ERROR_IO_PENDING;
-use windows_sys::Win32::Storage::FileSystem::{CreateFileW, ReadFile};
-use windows_sys::Win32::System::IO::CancelIo;
+use windows_sys::Win32::Storage::FileSystem::{ReadFile, WriteFile};
 use windows_sys::Win32::{Foundation::HANDLE, Storage::FileSystem::FILE_FLAG_OVERLAPPED};
 
 use std::fs::{File as StdFile, Metadata, OpenOptions as StdOpenOptions, Permissions};
@@ -12,7 +11,7 @@ use std::ptr::null_mut;
 use crate::context::IOType;
 use crate::io::Read;
 use crate::utils::{cvt, len};
-use crate::{AsHandle, Context};
+use crate::{AsHandle, Context, Write, ReadAt, WriteAt};
 
 pub struct OpenOptions {
     opt: StdOpenOptions,
@@ -116,6 +115,40 @@ impl File {
     pub fn set_permissions(&self, perm: Permissions) -> Result<()> {
         self.handle.set_permissions(perm)
     }
+
+    fn _read(&mut self, mut buff: Vec<u8>, offset: u64) -> Result<Context> {
+        let len = len(&buff);
+        let buff_ptr = buff.as_mut_ptr();
+        let handle = self.handle.as_raw_handle() as HANDLE;
+        let mut context = Context::new(handle, buff, IOType::Read);
+        let over_lapped_ptr = context.over_lapped_ptr();
+        context.set_offset(offset);
+
+        let ret = unsafe { ReadFile(handle, buff_ptr as *mut _, len, null_mut(), over_lapped_ptr) };
+
+        match cvt(ret) {
+            Ok(_) => Ok(context),
+            Err(e) if e.raw_os_error() == Some(ERROR_IO_PENDING as i32) => Ok(context),
+            Err(e) => Err(e),
+        }
+    }
+
+    fn _write(&self, buff: Vec<u8>, offset: u64) -> Result<Context> {
+        let len = len(&buff);
+        let buff_ptr = buff.as_ptr();
+        let handle = self.handle.as_raw_handle() as HANDLE;
+        let mut context = Context::new(handle, buff, IOType::Write);
+        let over_lapped_ptr = context.over_lapped_ptr();
+        context.set_offset(offset);
+
+        let ret = unsafe { WriteFile(handle, buff_ptr, len, null_mut(), over_lapped_ptr) };
+
+        match cvt(ret) {
+            Ok(_) => Ok(context),
+            Err(e) if e.raw_os_error() == Some(ERROR_IO_PENDING as i32) => Ok(context),
+            Err(e) => Err(e),
+        }
+    }
 }
 
 impl AsHandle for File {
@@ -143,47 +176,58 @@ impl Read for File {
     ///     let mut list = vec![context];
     ///
     ///     let result_list = cmp.get_many(&mut list, None)?;
-    ///
+    ///     let (buff, size, _io_type) = result_list.remove(0).get();
+    ///     assert_eq!(&buff[..size], b"123sdf");
+    ///     assert_eq!(&size, 6);
     ///     Ok(())
     /// }
     /// ```
-    fn read(&mut self, mut buff: Vec<u8>) -> Result<Context> {
-        let len = len(&buff);
-        let buff_ptr = buff.as_mut_ptr();
-        let mut context = Context::new(self.handle.as_raw_handle() as HANDLE, buff, IOType::Read);
-        let over_lapped_ptr = context.over_lapped_ptr();
-
-        let ret = unsafe {
-            ReadFile(
-                self.handle.as_raw_handle() as HANDLE,
-                buff_ptr as *mut _,
-                len,
-                null_mut(),
-                over_lapped_ptr,
-            )
-        };
-
-        match cvt(ret) {
-            Ok(_) => Ok(context),
-            Err(e) if e.raw_os_error() == Some(ERROR_IO_PENDING as i32) => Ok(context),
-            Err(e) => Err(e),
-        }
+    fn read(&mut self, buff: Vec<u8>) -> Result<Context> {
+        self._read(buff, 0)
     }
 }
 
-impl Drop for File {
-    fn drop(&mut self) {
-        let ret = unsafe { CancelIo(self.handle.as_raw_handle() as HANDLE) };
+impl ReadAt for File {
+    fn read_at(&mut self, buff: Vec<u8>, offset: u64) -> Result<Context> {
+        self._read(buff, offset)
+    }
+}
 
-        if ret == 0 {
-            panic!("file drop error {:?}", Error::last_os_error());
-        }
+impl Write for File {
+    ///
+    /// ```
+    /// use iocp_rs::{CompletionPort, File, OpenOptions, Write};
+    /// use std::io::Result;
+    /// use std::path::Path;
+    /// 
+    /// fn main() -> Result<()> {
+    ///     let cmp = CompletionPort::new(1)?;
+    ///     let mut file = OpenOptions::new().read(true).create_new(true).open("./tmp.txt")?;
+    ///     cmp.add(1, &file)?;
+    ///     let mut buff = b"123".to_vec();
+    ///     
+    ///     let context = file.write(buff)?;
+    ///     let mut list = vec![context];
+    ///
+    ///     let result_list = cmp.get_many(&mut list, None)?;
+    ///     let (buff, size, _io_type) = result_list.remove(0).get();
+    ///     Ok(())
+    /// }
+    /// ```
+    fn write(&self, buff: Vec<u8>) -> Result<Context> {
+        self._write(buff, 0)
+    }
+}
+
+impl WriteAt for File {
+    fn write_at(&self, buff: Vec<u8>, offset: u64) -> Result<Context> {
+        self._write(buff, offset)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{CompletionPort, OpenOptions, Read};
+    use crate::{CompletionPort, OpenOptions, Read, Write};
 
     #[test]
     fn read_file() {
@@ -193,11 +237,33 @@ mod tests {
             .open("E:\\rust\\iocp-rs\\test.txt")
             .unwrap();
         cmp.add(1, &file).unwrap();
-        let mut buff = vec![0; 10];
+        let buff = vec![0; 10];
 
         let context = file.read(buff).unwrap();
         let mut list = vec![context];
 
-        let result_list = cmp.get_many(&mut list, None).unwrap();
+        let mut result_list = cmp.get_many(&mut list, None).unwrap();
+        let (buff, size, _io_type) = result_list.remove(0).get();
+        assert_eq!(&buff[..size], b"123sdf");
+        assert_eq!(size, 6);
+    }
+
+    #[test]
+    fn write_file() {
+        let cmp = CompletionPort::new(1).unwrap();
+        let file = OpenOptions::new()
+            .write(true)
+            .open("E:\\rust\\iocp-rs\\test.txt")
+            .unwrap();
+        cmp.add(1, &file).unwrap();
+        let buff = b"123".to_vec();
+
+        let context = file.write(buff).unwrap();
+        let mut list = vec![context];
+
+        let mut result_list = cmp.get_many(&mut list, None).unwrap();
+        let (buff, size, _io_type) = result_list.remove(0).get();
+        assert_eq!(&buff, b"123");
+        assert_eq!(size, 3);
     }
 }
--- src/io.rs
+++ src/io.rs
@@ -5,3 +5,15 @@ use crate::Context;
 pub trait Read {
     fn read(&mut self, buff: Vec<u8>) -> Result<Context>;
 }
+
+pub trait ReadAt {
+    fn read_at(&mut self, buff: Vec<u8>, offset: u64) -> Result<Context>;
+}
+
+pub trait Write {
+    fn write(&self, buff: Vec<u8>) -> Result<Context>;
+}
+
+pub trait WriteAt {
+    fn write_at(&self, buff: Vec<u8>, offset: u64) -> Result<Context>;
+}
